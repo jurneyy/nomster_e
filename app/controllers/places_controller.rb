dc3ed33d50a52c912a490d2d32fe37fc47e456a0
Erny Juarez <ernyjuarez@gmail.com>
2019-07-11 01:19:07 +0000

    updated form connected to db

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -14,6 +14,26 @@ class PlacesController < ApplicationController
 
 	end
 
+
+
+	def create
+
+		Place.create(place_params)
+
+		redirect_to root_path
+
+	end
+
+
+	private
+
+
+	def place_params
+
+		params.require(:place).permit(:name, :description, :address)
+
+	end
+
 end
 
 
