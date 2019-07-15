3046c6ef3caadcc5b66cf155329083130a51db1b
Erny Juarez <ernyjuarez@gmail.com>
2019-07-15 20:58:14 +0000

    updated edit function

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -40,6 +40,15 @@ class PlacesController < ApplicationController
 	end
 
 
+	def update
+
+		@place = Place.find(params[:id])
+		@place.update_attributes(place_params)
+		redirect_to root_path
+
+	end
+
+
 	private
 
 
