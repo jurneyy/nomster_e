48c0acd6134ca6b0a78c5061df1c0d8672565a87
Erny Juarez <ernyjuarez@gmail.com>
2019-07-21 17:04:51 +0000

    updated photocntl.rb

--- db/schema.rb
+++ db/schema.rb
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 2019_07_20_015645) do
 
   create_table "photos", force: :cascade do |t|
     t.text "caption"
+    t.integer "user_id"
     t.integer "place_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
