6c34096a2268d766ca2aacc2a3b16ea7c41182e8
Erny Juarez <ernyjuarez@gmail.com>
2019-07-24 03:01:40 +0000

    email not being received

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,12 +1,8 @@
 class NotificationMailer < ApplicationMailer
-	default from: "no-reply@nomsterapp.com"
+  default from: "no-reply@nomsterapp.com"
 
-	def comment_added
-
-		mail(to: "ernyjuarez@gmail.com", 
-
-			subject: "A comment has been added to your place")
-
-	end
-
+  def comment_added
+    mail(to: "ernyjuarez@gmail.com",
+         subject: "A comment has been added to your place")
+  end
 end
