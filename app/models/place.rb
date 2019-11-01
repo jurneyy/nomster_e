b7668bdfbef63c87e386f10caa78e6bad285c14a
Erny Juarez <ernyjuarez@gmail.com>
2019-11-01 21:06:09 +0000

    updated Css and routes

--- app/models/place.rb
+++ app/models/place.rb
@@ -10,5 +10,4 @@ class Place < ApplicationRecord
 	validates :name, presence: true, length: { minimum: 3 } 
 	validates :address, presence: true
 	validates :description, presence: true
-
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,5 +7,4 @@ class User < ApplicationRecord
   has_many :places
   has_many :comments
   has_many :photos
-  
 end
