bdd2420935f628a2915a8891fbcd95cc87253045
Erny Juarez <ernyjuarez@gmail.com>
2019-11-02 20:39:34 +0000

    updated syntax on db migration files

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -14,15 +14,11 @@ class Comment < ApplicationRecord
   }
 
 	def humanized_rating
-
 		RATINGS.invert[self.rating]
-
 	end
 
 	def send_comment_email
-
 		NotificationMailer.comment_added(self).deliver_now
-
 	end
 
 end
--- db/migrate/20190718014444_create_comments.rb
+++ db/migrate/20190718014444_create_comments.rb
@@ -8,11 +8,8 @@ class CreateComments < ActiveRecord::Migration[5.2]
     	t.integer :user_id
     	t.integer :place_id
     	t.timestamps
-    	
     end
-
     add_index :comments, [:user_id, :place_id]
     add_index :comments, :place_id
-
   end
 end
